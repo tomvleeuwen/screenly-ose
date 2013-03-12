b57ffe661dd91cd533e9092ea1a87528b0a0d27a
schildt <schildt@ibr.cs.tu-bs.de>
2013-03-12 16:20:52 +0000

    Working beaconing

--- beacon.py
+++ beacon.py
@@ -1,31 +1,45 @@
 import uuid
-import socket
 import httplib, urllib
-import time
-import signal
+import time, datetime
+from settings import settings, DEFAULTS
+#http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+import socket, struct, fcntl
 
 
-def sigusr1(signum, frame):
-	print("Sigusr catched")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockfd = sock.fileno()
+SIOCGIFADDR = 0x8915
 
+def get_ip(iface = 'eth0'):
+     ifreq = struct.pack('16sH14s', iface, socket.AF_INET, '\x00'*14)
+     try:
+         res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
+     except:
+         return None
+     ip = struct.unpack('16sH2x4s8x', res)[2]
+     return socket.inet_ntoa(ip)
+
+
+ 
+ip=get_ip('eth0')
 uuid=uuid.getnode()
-ip=[ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1]
+
+sleep=30
+
+mothership=settings['mothership']
+port=settings.get_listen_port()
 
 print("UUID is "+str(uuid)+" for IP "+str(ip))
+print("Mothership location is at "+str(mothership))
 
+while True:
+	params = urllib.urlencode({'uuid': uuid, 'ip': str(ip)+":"+str(port)})
+	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+	conn = httplib.HTTPConnection(mothership)
+	conn.request("POST", "/beacon", params, headers)
+	response = conn.getresponse()
+	print(str(datetime.datetime.now())+" Beacon: "+str(response.status)+" ("+str(response.reason)+")")
+        conn.close()
+	time.sleep(sleep)
 
-params = urllib.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
-headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-conn = httplib.HTTPConnection("bugs.python.org")
-conn.request("POST", "", params, headers)
-response = conn.getresponse()
-print response.status, response.reason
-data = response.read()
-print (str(data))
-conn.close()
-
-
-signal.signal(signal.SIGUSR1, sigusr1)
-print("Waiting ...")
-time.sleep(60)
-print("Done")
+print("Beaconing stop")
