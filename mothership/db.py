1507743336ddf0f1b092216666bd6548b2647a93
Sebastian <schildt@ibr.cs.tu-bs.de>
2013-07-23 17:49:18 +0200

    Updated db scheme

--- mothership/db.py
+++ mothership/db.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 CREATE_TABLE="""CREATE TABLE IF NOT EXISTS pod (
                  uuid        TEXT     PRIMARY KEY UNIQUE, 
+		 label TEXT,
                  destination TEXT, 
                  lastseen    DATETIME 
                  );"""
@@ -27,12 +28,26 @@ def addEntry(uuid,dst):
 	timestamp=datetime.now()
 	timestr=timestamp.strftime("%Y-%m-%d %H:%M:%S")
 	cur = conn.cursor()
-	cur.execute("INSERT OR REPLACE INTO pod VALUES(?,?,?)", (str(uuid), str(dst),str(timestr)));
+	cur.execute("INSERT OR REPLACE INTO pod(uuid,destination,lastseen) VALUES(?,?,?)", (str(uuid), str(dst),str(timestr)));
 	conn.commit()
+
+
+def updateLabel(uuid,label):
+	global conn
+	cur = conn.cursor()
+	cur.execute("UPDATE pod SET label=? WHERE uuid=?", (str(label), str(uuid)));
+	conn.commit()
+	
+def delete(uuid):
+	global conn
+	cur = conn.cursor()
+	cur.execute("DELETE from pod WHERE uuid=?", ([uuid]));
+	conn.commit()
+
 
 def getAll():
 	global conn
 	cur = conn.cursor()
-	cur.execute("SELECT * from pod");
+	cur.execute("SELECT uuid,label,destination,lastseen from pod");
 	return cur.fetchall()
 	
