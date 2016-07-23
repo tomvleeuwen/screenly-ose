cb561a4397add56d2038aba1f1108e55e344f438
Sebastian Schildt <sebastian@frozenlight.de>
2016-07-23 22:26:50 +0200

    Create folder for auth not exists, set PW form command line

--- misc/createAuth.py
+++ misc/createAuth.py
@@ -5,16 +5,24 @@
 
 from datetime import datetime
 from cork import Cork
+import sys
+import os
 
-def populate_conf_directory():
+def populate_conf_directory(adminpass):
+    print("Setting screenly admin pw to "+str(adminpass))
+    if not os.path.exists("/home/pi/screenly.auth"):
+        os.mkdir("/home/pi/screenly.auth")
+    
     cork = Cork('/home/pi/screenly.auth', initialize=True)
 
     cork._store.roles['admin'] = 100
     cork._store.roles['editor'] = 60
     cork._store.save_roles()
 
-    tstamp = str(datetime.utcnow())
-    username = password = 'admin'
+    tstamp = str(datetime.utcnow()) 
+    username = 'admin'
+    password=adminpass
+    
     cork._store.users[username] = {
         'role': 'admin',
         'hash': cork._hash(username, password),
@@ -33,5 +41,9 @@ def populate_conf_directory():
     cork._store.save_users()
 
 if __name__ == '__main__':
-    populate_conf_directory()
+    if len(sys.argv) == 1:
+        pw="admin"
+    else:
+        pw=sys.argv[1]
+    populate_conf_directory(pw)
 
