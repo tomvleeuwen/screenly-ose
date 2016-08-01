acad475c5b2bf72231cfcd5036b1350600548354
tomvleeuwen <github@tomvanleeuwen.nl>
2016-08-01 20:58:00 +0200

    CEC-settings, try2

--- server.py
+++ server.py
@@ -111,8 +111,8 @@ def template(template_name, **context):
     context['remote_host'] = '"'+settings['remote_host']+'"'
     context['remote_port'] = settings['remote_port']
     context['remote_enabled'] = settings['remote_enabled']
-    context['tv_power_on'] = settings['power_on']
-    context['tv_active_source'] = settings['active_source']
+    context['tv_power_on'] = settings['tv_power_on']
+    context['tv_active_source'] = settings['tv_active_source']
 
     return haml_template(template_name, **context)
 
--- settings.py
+++ settings.py
@@ -32,14 +32,14 @@ DEFAULTS = {
         'mothership': 'mothership:8080',
     },
     'cec': {
-        'power_on' : True,
-        'active_source' : True
+        'tv_power_on' : True,
+        'tv_active_source' : True
     }
 }
 CONFIGURABLE_SETTINGS = DEFAULTS['viewer']
 CONFIGURABLE_SETTINGS['use_24_hour_clock'] = DEFAULTS['main']['use_24_hour_clock']
-CONFIGURABLE_SETTINGS['power_on'] = DEFAULTS['cec']['power_on']
-CONFIGURABLE_SETTINGS['active_source'] = DEFAULTS['cec']['active_source']
+CONFIGURABLE_SETTINGS['tv_power_on'] = DEFAULTS['cec']['tv_power_on']
+CONFIGURABLE_SETTINGS['tv_active_source'] = DEFAULTS['cec']['tv_active_source']
 
 # Initiate logging
 logging.basicConfig(level=logging.INFO,
